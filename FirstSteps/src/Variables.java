83d71b3887e717fea6326f63c666d7d810359a80
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-03 06:24:42 +0200

    2024/08/03(am)
    Variables - Example Completed

--- FirstSteps/src/Variables.java
+++ FirstSteps/src/Variables.java
@@ -1,5 +1,19 @@
 public class Variables {
     public static void main(String[] args) {
-        
+
+       int number = 1;
+       double number2 = 1.234d;
+       float number3 = 1.234f;
+       String name = "Conrad";
+       boolean trueFalse = true;
+       char Character = 'A';
+
+        System.out.println("Integer (whole number): " + number + "\n" +
+                "Double (Number with decimal places): " + number2 + "\n" +
+                "Float (Number with decimal places): " + number3 + "\n" +
+                "String (Word or Sentence): " + name + "\n" +
+                "Boolean (Statement that indicates true or false): " + trueFalse + "\n" +
+                "Char (Single character): " + Character);
+
     }
 }
